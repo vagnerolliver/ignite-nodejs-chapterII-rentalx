3ba479bd7433282207a36f3863a99abb6d45dbe4
Vagner Oliveira <vagner.olliver@yahoo.com.br>
2022-08-28 15:41:49 -0300

    add config debug

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from "express";
 
 const app = express();
-
+app.use(express.json());
 app.get("/", (req, res) => {
   return res.json({ message: "Hello" });
+});
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
 });
 
 app.listen(3333, () => console.log("server is running"));

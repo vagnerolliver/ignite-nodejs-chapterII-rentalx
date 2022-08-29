0ad8c5fe047295c5eee04ffd2e06ba8ca321ca5e
Vagner Oliveira <vagner.olliver@yahoo.com.br>
2022-08-29 09:22:39 -0300

    feat: category routes

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
+
 app.use(express.json());
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello" });
-});
 
-app.post("/courses", (req, res) => {
-  const { name } = req.body;
-
-  return res.json({ name });
-});
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("server is running"));

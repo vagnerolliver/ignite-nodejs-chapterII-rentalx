145552227355aea5ba8e29b6f030ca4777a043fc
Vagner Oliveira <vagner.olliver@yahoo.com.br>
2022-09-15 10:09:22 -0300

    refact: specification useCases

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,21 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationRepository } from "../modules/cars/repositories/implementations/SpecificationRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
-const speficationRepository = new SpecificationRepository();
 
 specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    speficationRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return response.status(201).send();
+  return createSpecificationController.handle(request, response);
 });
 
 export { specificationsRoutes };
